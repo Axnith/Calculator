1d0c1078dfd93c8f7e39c4614f96dae7b08f3707
Asnith <121656672+Axnith@users.noreply.github.com>
2025-08-14 15:16:44 +0530

    Update Calculator.js

--- Calculator.js
+++ Calculator.js
@@ -13,8 +13,30 @@ function Evaluate(){
     display.value = 'Error';
     setTimeout(()=>{
       display.value = '';
-    },800);
-  }
+    },400);
+  }                    
 }
+document.addEventListener("keydown",(event)=>{
+  const key = event.key;
+
+  if(!isNaN(key) || key==='.'){
+    AppendToDisplay(key);
+  }
+   if(['+','-','*','/'].includes(key)){
+    AppendToDisplay(key);
+  }
+   if(key==='c'||key==='C'){
+    ClearDisplay();
+  }
+   if(key==='Backspace'){
+    display.value = display.value.slice(0,-1);
+  }
+  if(key==='Enter'||key==='='){
+    Evaluate();
+  }
+  if(key==='('||key===')'){
+    AppendToDisplay(key);
+  }
+});
 
 
