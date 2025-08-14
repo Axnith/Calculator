4460046c07ddde083715d531fe6da7c79bfd1b0f
Asnith <121656672+Axnith@users.noreply.github.com>
2025-08-14 14:26:17 +0530

    Add files via upload

---
+++ Calculator.js
@@ -0,0 +1,19 @@
+const display = document.getElementById("output-display");
+function AppendToDisplay(input){
+  display.value+=input;
+}
+function ClearDisplay(){
+  display.value = '';
+}
+function Evaluate(){
+  try{
+    display.value = eval(display.value);
+  }
+  catch{
+    display.value = 'Error';
+    setTimeout(()=>{
+      display.value = '';
+    },2000);
+  }
+}
+
